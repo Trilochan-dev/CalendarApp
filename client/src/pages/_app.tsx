8cb609322a4646e1e5ee77b289c934cb80a02120
Trilochan <127761645+trilochan-behera-dev@users.noreply.github.com>
2023-07-28 00:41:15 +0530

    Update _app.tsx

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import Calendar from "../components/Calendar/Calendar";
 import "../styles/globals.css";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { addDays, startOfWeek } from "date-fns";
 
 export const CalendarContext = createContext(null);
@@ -9,6 +9,9 @@ export default function App({ Component, pageProps }) {
   const router = useRouter();
   const [lastDate, setlastDate] = useState(addDays(startOfWeek(new Date()), 0));
   const [userinfo, setUserinfo] = useState({});
+  const [totalDays, setTotalDays] = useState(7);
+  const [apiCall, setApiCall] = useState(false)
+
   useEffect(() => {
     const token = JSON.parse(localStorage.getItem("login_token"));
     const userdata = JSON.parse(localStorage && localStorage.getItem("user_info"));
@@ -22,12 +25,41 @@ export default function App({ Component, pageProps }) {
       router.push("/");
     }
   }, [router]);
+
+  const setTotalDaysShown = () => {
+    const width = window.innerWidth;
+    if (width >= 1024) {
+      return 7;
+    } else if (width >= 768) {
+      return 4;
+    } else {
+      return 1;
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setTotalDays(setTotalDaysShown())
+    };
+
+    // Add event listener to handle window resize
+    window.addEventListener("resize", handleResize);
+    setTotalDays(setTotalDaysShown())
+
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   return (
     <>
       {router.pathname === "/login" || router.pathname === "/register" ? (
         <Component {...pageProps} />
       ) : (
-        <CalendarContext.Provider value={{ lastDate, setlastDate, userinfo }}>
+        <CalendarContext.Provider value={{ lastDate, setlastDate, userinfo, totalDays, apiCall, setApiCall }}>
           <Calendar />
         </CalendarContext.Provider>
       )}
